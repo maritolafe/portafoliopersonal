d7b5b91b677eaf18203cf462e8f5dbf9f9a6e5c3
marito <zalazar.unlz@gmail.com>
2025-05-23 22:40:43 -0300

    portafolio23052025

--- portafolio/js/menu.js
+++ portafolio/js/menu.js
@@ -1,4 +1,15 @@
 function toggleMenu() {
   const menu = document.getElementById("lista");
+  const abrir = document.getElementById("botonMenu");
+  const cerrar = document.getElementById("botonCerrar");
+
   menu.classList.toggle("visible");
+
+  if (menu.classList.contains("visible")) {
+    abrir.style.display = "none";
+    cerrar.style.display = "inline-block";
+  } else {
+    abrir.style.display = "inline-block";
+    cerrar.style.display = "none";
+  }
 }
